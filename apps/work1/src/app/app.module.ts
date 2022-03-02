d1df5a97d229be9f178c982e1f5af2a4ece3cdd4
Gergo Nagy <dzsi1994>
2022-03-02 16:00:40 +0100

    generate auth interceptor.

--- apps/work1/src/app/app.module.ts
+++ apps/work1/src/app/app.module.ts
@@ -1,13 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { SharedUiCardModule } from '@shared-ui/card';
+import { AuthInterceptor } from '@core/auth';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, SharedUiCardModule],
-  providers: [],
+  imports: [BrowserModule, SharedUiCardModule, HttpClientModule],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      multi: true,
+      useClass: AuthInterceptor,
+    },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
---
+++ libs/core/auth/src/index.ts
@@ -0,0 +1,2 @@
+export * from './lib/core-auth.module';
+export { AuthInterceptor } from './lib/core-auth.interceptor';
